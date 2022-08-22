1245ab161c39c9ea62b4bcf58798583f899e8b1a
phuc <phamhuulequocphuc@gmail.com>
2022-08-22 20:36:27 +0700

    route/params

---
+++ src/pages/route/params/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    {{ id }} >>> Next: <NuxtLink :to="`/route/params/${id + 1}`">{{ id + 1 }}</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const id = ref<number>(route.params.id as unknown as number)
+</script>
