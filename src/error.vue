1e91c590359ffde148b93506536fe1382ccf88cd
phuc <phamhuulequocphuc@gmail.com>
2022-08-22 21:13:39 +0700

    enhance

---
+++ src/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{ error.message }}</p>
+  <button @click="handleError">
+    Clear errors
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
---
+++ src/pages/use-pug.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const headers = useState<string>('headers', () => 'I\'m pug')
+</script>
+
+<template lang="pug">
+h1 PUG template
+div headers: {{ headers }}
+ul
+  li(v-for="i in 10")
+    NuxtLink(:to="`/route/params/${i}`") {{ i }}
+</template>
